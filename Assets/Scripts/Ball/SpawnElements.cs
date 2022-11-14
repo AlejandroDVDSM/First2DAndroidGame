0d31c98d01206d61d280e16d5c9731a1d8e4fabf
AlejandroDVDSM <bysalexx@gmail.com>
2022-11-14 09:34:05 +0000

    Game now respawns all elements instead of a ball

--- Assets/Scripts/Ball/SpawnElements.cs
+++ Assets/Scripts/Ball/SpawnElements.cs
@@ -3,21 +3,45 @@ using UnityEngine;
 public class SpawnElements : MonoBehaviour
 {
     [SerializeField] GameObject elementPrefab;
-    [SerializeField] float minValueAllowed;
-    [SerializeField] float maxValueAllowed;
+    [SerializeField] float minValueAllowed, maxValueAllowed;
+    [SerializeField] GameObject[] badElements, goodElements, upgrades;
 
-    Vector2 respawnPos;
     float randomX;
+    Vector2 respawnPos;
+    int randomElement;
 
     void Start()
     {
-        InvokeRepeating("SpawnElement", 1.0f, 1.5f);
+        InvokeRepeating("SpawnElement", 1.0f, 1.65f);
     }
 
     void SpawnElement()
     {
         randomX = Random.Range(minValueAllowed, maxValueAllowed);
-        respawnPos = new Vector2(randomX, elementPrefab.transform.position.y);
-        Instantiate(elementPrefab, respawnPos, transform.rotation);
+        respawnPos = new Vector2(randomX, transform.position.y);
+        randomElement = Random.Range(0, badElements.Length);
+        int typeOfElement = Random.Range(-1, 2);
+        ChooseElementToSpawn(typeOfElement);
+    }
+
+    void ChooseElementToSpawn(int typeOfElement)
+    {
+        switch (typeOfElement)
+        {
+            case -1:
+                randomElement = Random.Range(0, badElements.Length);
+                Instantiate(badElements[randomElement], respawnPos, transform.rotation);
+                break;
+
+            case 0:
+                randomElement = Random.Range(0, goodElements.Length);
+                Instantiate(goodElements[randomElement], respawnPos, transform.rotation);
+                break;
+
+            case 1:
+                randomElement = Random.Range(0, upgrades.Length);
+                Instantiate(upgrades[randomElement], respawnPos, transform.rotation);
+                break;
+        }
     }
 }
