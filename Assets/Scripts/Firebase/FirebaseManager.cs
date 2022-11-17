c1cbe33f7ea545feea5fc631840f68fdc8231597
AlejandroDVDSM <bysalexx@gmail.com>
2022-11-17 12:21:40 +0000

    Auth user

---
+++ Assets/Scripts/Firebase/FirebaseManager.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using Firebase;
+using Firebase.Auth;
+
+public class FirebaseManager : MonoBehaviour
+{
+    FirebaseApp _app;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == DependencyStatus.Available)
+            {
+                // Create and hold a reference to your FirebaseApp,
+                // where app is a Firebase.FirebaseApp property of your application class.
+                _app = FirebaseApp.DefaultInstance;
+
+                // Set a flag here to indicate whether Firebase is ready to use by your app.
+                Login();
+            }
+            else
+            {
+                Debug.LogError(System.String.Format(
+                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+            }
+        });
+    }
+
+    void Login()
+    {
+        FirebaseAuth auth = FirebaseAuth.DefaultInstance;
+
+        if (auth.CurrentUser != null)
+        {
+            Debug.Log("{ " + auth.CurrentUser.UserId + " } user is already authenticated");
+            return;
+        }
+
+        auth.SignInAnonymouslyAsync().ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInAnonymouslyAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInAnonymouslyAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            FirebaseUser newUser = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
+    }
+}
