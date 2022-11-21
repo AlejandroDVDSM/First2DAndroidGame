0fe3136d97b3ff0ad953334992428414db5f2186
AlejandroDVDSM <bysalexx@gmail.com>
2022-11-21 13:50:46 +0000

    Add timer UI

---
+++ Assets/Scripts/UI/Timer.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI _timerText;
+
+    private float timeInSeconds = 60;
+}
