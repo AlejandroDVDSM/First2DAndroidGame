a7e102bbd54115ed47c94d9c1d88f348047cdb3e
AlejandroDVDSM <bysalexx@gmail.com>
2022-11-22 09:25:08 +0000

    Add funcionality to the timer

--- Assets/Scripts/UI/Timer.cs
+++ Assets/Scripts/UI/Timer.cs
@@ -7,5 +7,23 @@ public class Timer : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI _timerText;
 
-    private float timeInSeconds = 60;
+    private float _timeInSeconds = 3;
+
+    void Update()
+    {
+
+        if (_timeInSeconds > 0) _timeInSeconds -= Time.deltaTime;
+
+        DisplayTime(_timeInSeconds);
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        if (timeToDisplay < 0) timeToDisplay = 0;
+
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        _timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
 }
