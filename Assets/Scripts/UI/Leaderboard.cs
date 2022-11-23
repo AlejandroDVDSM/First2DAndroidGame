6bc24be4bf51b469e1641dd9c0189a8f9d826e5f
AlejandroDVDSM <bysalexx@gmail.com>
2022-11-23 11:48:40 +0000

    Load the data of firebase in the leaderboard

--- Assets/Scripts/UI/Leaderboard.cs
+++ Assets/Scripts/UI/Leaderboard.cs
@@ -35,18 +35,28 @@ public class Leaderboard : MonoBehaviour
 
             var newRowPosX = _content.transform.position.x;
             var newRowPosY = _content.transform.position.y;
+            var newRowPos = new Vector3(newRowPosX, newRowPosY, 0);
             var rank = 1;
 
             for (var i = snapshot.ChildrenCount - 1; i >= 0; i--)
             {
                 Debug.Log( ((Dictionary<string, object>)snapshotsList[(int)i].Value)["userName"] + " - " + ((Dictionary<string, object>)snapshotsList[(int)i].Value)["userScore"]);
 
-                var newRowPos = new Vector3(newRowPosX, newRowPosY, 0);
-                newRowPosY -= 20;
                 var newRow = Instantiate(_row, newRowPos, _row.transform.rotation, _content.transform);
+                newRowPos.y -= 20;
+
                 newRow.name = _row.name + "-" + rank;
+
+                var userRank = newRow.transform.GetChild(0).gameObject;
+                userRank.GetComponent<TextMeshProUGUI>().text = rank + "";
+
+                var userName = newRow.transform.GetChild(1).gameObject;
+                userName.GetComponent<TextMeshProUGUI>().text = ((Dictionary<string, object>)snapshotsList[(int)i].Value)["userName"].ToString();
+
+                var userScore = newRow.transform.GetChild(2).gameObject;
+                userScore.GetComponent<TextMeshProUGUI>().text = ((Dictionary<string, object>)snapshotsList[(int)i].Value)["userScore"].ToString();
+
                 rank++;
-                //newRow.transform.SetParent(_content.transform, false);
             }
         }
     }
