3596f08faf3ef891d67fecc4bb39fac70d15ecec
AlejandroDVDSM <bysalexx@gmail.com>
2022-11-24 13:16:53 +0000

    Allow the player to select a language

---
+++ Assets/Scripts/UI/LocateSelector.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Localization.Settings;
+using TMPro;
+
+public class LocateSelector : MonoBehaviour
+{
+    [SerializeField] TMP_Dropdown _languagueDropdown;
+    bool _active = false;
+
+    public void ChangeLocale()
+    {
+        if (_active) return;
+
+        StartCoroutine(SetLocale(_languagueDropdown.value));
+    }
+
+
+    IEnumerator SetLocale(int localeID)
+    {
+        _active = true;
+        yield return LocalizationSettings.InitializationOperation;
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[localeID];
+        _active = false;
+    }
+}
