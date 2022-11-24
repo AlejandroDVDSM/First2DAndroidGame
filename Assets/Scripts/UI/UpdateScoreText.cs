7e19085f2921cd03cffdf888c5ac7e86e75678ac
AlejandroDVDSM <bysalexx@gmail.com>
2022-11-24 11:33:59 +0000

    Add Unity Localization and show a few translations in game

--- Assets/Scripts/UI/UpdateScoreText.cs
+++ Assets/Scripts/UI/UpdateScoreText.cs
@@ -2,23 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.Localization;
 
 public class UpdateScoreText : MonoBehaviour
 {
     [SerializeField] ScoreManager _scoreManagerScript;
     int _currentScore;
-    TextMeshProUGUI _scoreText;
 
-    void Start()
-    {
-        _scoreText = gameObject.GetComponent<TextMeshProUGUI>();
-    }
-
+    [SerializeField] LocalizedString _localStringScore;
+    [SerializeField] TextMeshProUGUI _scoreText;
 
     // Update is called once per frame
     void Update()
     {
         _currentScore = _scoreManagerScript.Score;
-        _scoreText.text = _currentScore + " points";
+        _localStringScore.Arguments[0] = _currentScore;
+        _localStringScore.RefreshString();
+    }
+
+    void OnEnable()
+    {
+        _localStringScore.Arguments = new object[] { _scoreManagerScript.Score };
+        _localStringScore.StringChanged += UpdateText;
+    }
+
+    void OnDisable()
+    {
+        _localStringScore.StringChanged -= UpdateText;
+    }
+
+    void UpdateText(string value)
+    {
+        _scoreText.text = value;
     }
 }
