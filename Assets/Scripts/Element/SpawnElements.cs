52082f13995bffb63d1f45f1561306fbe6e9e9b3
AlejandroDVDSM <bysalexx@gmail.com>
2022-11-25 11:51:53 +0000

    Correct display UI according to languague

--- Assets/Scripts/Element/SpawnElements.cs
+++ Assets/Scripts/Element/SpawnElements.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class SpawnElements : MonoBehaviour
 {
     [SerializeField] float _minXAllowed, _maxXAllowed;
-    [SerializeField] GameObject[] _badElements, _goodElements /*, _upgrades*/;
+    [SerializeField] GameObject[] _badElements, _goodElements;
 
     float _randomX;
     Vector2 _respawnPos;
@@ -18,7 +18,7 @@ public class SpawnElements : MonoBehaviour
     {
         _randomX = Random.Range(_minXAllowed, _maxXAllowed);
         _respawnPos = new Vector2(_randomX, transform.position.y);
-        int typeOfElement = Random.Range(-1, 1); // Change max range from 1 to 2 if upgrades will be used
+        int typeOfElement = Random.Range(-1, 1);
         ChooseElementToSpawn(typeOfElement);
     }
 
@@ -28,18 +28,14 @@ public class SpawnElements : MonoBehaviour
         {
             case -1:
                 _randomElement = Random.Range(0, _badElements.Length);
-                Instantiate(_badElements[_randomElement], _respawnPos, transform.rotation);
+                var badElement = Instantiate(_badElements[_randomElement], _respawnPos, transform.rotation);
                 break;
 
             case 0:
                 _randomElement = Random.Range(0, _goodElements.Length);
-                Instantiate(_goodElements[_randomElement], _respawnPos, transform.rotation);
+                var goodElement = Instantiate(_goodElements[_randomElement], _respawnPos, transform.rotation);
+                goodElement.GetComponent<Rigidbody2D>().gravityScale = .9f;
                 break;
-
-            /*case 1:
-                _randomElement = Random.Range(0, _upgrades.Length);
-                Instantiate(_upgrades[_randomElement], _respawnPos, transform.rotation);
-                break;*/
         }
     }
 }
